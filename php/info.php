60955e1c5e982be583c274ba1244aa6ad6958256
anixer0 <anixer42@gmail.com>
2024-09-11 12:14:37 +0800

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,8 +1,8 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = ""; // add the api here with the '/' at the end!
+$apiLink = "https://animeapi-v8nu.onrender.comm"; // add the api here with the '/' at the end!
 if (date("d") > 15){
-    $apiLink = ""; // same goes for here
+    $apiLink = "https://animeapi-v8nu.onrender.com"; // same goes for here
 }
 ?>
